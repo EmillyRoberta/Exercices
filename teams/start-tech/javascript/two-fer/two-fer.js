8b344ea96a67c6e1c1afda22288b1732f4dc8e28
emilly.roberta <emilly.roberta@ambevtech.com.br>
2021-08-23 18:57:21 -0300

    Exercicio2- Two-fer

---
+++ teams/start-tech/javascript/two-fer/two-fer.js
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const twoFer = (name) => {
+  if(typeof name === 'undefined'){
+    return "One for you, one for me.";
+  } else{
+    return `One for ${name}, one for me.`;
+    }
+  }
+  
+
